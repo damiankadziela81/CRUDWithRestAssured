fa7e7be7f4e7964d435f71d864a57534522520e1
damiankadziela81 <damiankadziela02@gmail.com>
2023-07-04 12:24:41 +0200

    Add new version of 2nd test, introduced BASE_URL

--- src/test/java/org/example/ReadEmployeesTest.java
+++ src/test/java/org/example/ReadEmployeesTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +12,14 @@ import static io.restassured.RestAssured.given;
 
 class ReadEmployeesTest {
 
+    private static final String BASE_URL = "http://localhost:3000/employees";
+
     @Test
     void readAllEmployeesTest() {
 
         Response response = given()
                 .when()
-                .get("http://localhost:3000/employees");
+                .get(BASE_URL);
 
 //        This 2 will display response body on the console
 //        System.out.println(response.asString());
@@ -36,7 +39,7 @@ class ReadEmployeesTest {
 
         Response response = given()
                 .when()
-                .get("http://localhost:3000/employees/1");
+                .get(BASE_URL + "/1");
 
         Assertions.assertEquals(200,response.getStatusCode());
 
@@ -47,13 +50,28 @@ class ReadEmployeesTest {
         Assertions.assertEquals("8",json.getString("address.suite"));
     }
 
+    //without JsonPath
+    @Test
+    void readOneEmployeeV2Test() {
+
+        given()
+                .when()
+                .get(BASE_URL + "/1")
+                .then()
+                .statusCode(200)
+                .body("firstName", Matchers.equalTo("Bartek"))
+                .body("lastName", Matchers.equalTo("Czarny"))
+                .body("username", Matchers.equalTo("bczarny"))
+                .body("address.suite", Matchers.equalTo("8"));
+    }
+
     @Test
     void readOneUserWithPathVariableTest() {
 
         Response response = given()
                 .pathParams("id", 2)
                 .when()
-                .get("http://localhost:3000/employees/{id}");
+                .get(BASE_URL + "/{id}");
 
         Assertions.assertEquals(200,response.getStatusCode());
 
@@ -70,7 +88,7 @@ class ReadEmployeesTest {
         Response response = given()
                 .queryParam("firstName", "Bartek")
                 .when()
-                .get("http://localhost:3000/employees");
+                .get(BASE_URL);
 
         JsonPath json = response.jsonPath();
         //Note that the response will be a list, not a single object!
