3b1fd31ade4d3c6fc6dcc62ed804680931fb1ed2
damiankadziela81 <damiankadziela02@gmail.com>
2023-07-04 15:42:06 +0200

    Add new class for Delete method

---
+++ src/test/java/org/example/DeleteEmployeeTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class DeleteEmployeeTest {
+
+    @Test
+    void deleteEmployeeTest() {
+        Response response = given()
+                .when()
+                .delete("http://localhost:3000/employees/7")
+                .then()
+                .extract()
+                .response();
+
+        Assertions.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
+    }
+}
